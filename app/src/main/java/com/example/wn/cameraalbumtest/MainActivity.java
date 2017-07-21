481bd42b570f9ebc1e203e7a3c822f2b849ed38b
wn1137162270 <1137162270@qq.com>
2017-07-21 12:29:26 +0800

    add choose album

--- app/src/main/java/com/example/wn/cameraalbumtest/MainActivity.java
+++ app/src/main/java/com/example/wn/cameraalbumtest/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.wn.cameraalbumtest;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -18,6 +31,7 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     public static final int TAKE_PHOTO=1;
+    public static final int CHOOSE_PHOTO=2;
 
     private ImageView photo;
     private Uri imageUri;
@@ -28,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button takePhoto = (Button) findViewById(R.id.take_photo_btn);
+        Button choosePhoto= (Button) findViewById(R.id.choose_album_btn);
         photo= (ImageView) findViewById(R.id.photo_iv);
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +67,96 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,TAKE_PHOTO);
             }
         });
+        choosePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission
+                        .WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission
+                            .WRITE_EXTERNAL_STORAGE},1);
+                }
+                else {
+                    openAlbum();
+                }
+            }
+        });
+    }
+
+    private void openAlbum(){
+        Intent intent=new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,CHOOSE_PHOTO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions
+            , @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length>0&&PackageManager.PERMISSION_GRANTED==grantResults[0]){
+                    openAlbum();
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"You denied the permission",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private void handleImageOnKitKat(Intent data){
+        Uri uri=data.getData();
+        String imagePath=null;
+        if(DocumentsContract.isDocumentUri(MainActivity.this,uri)){
+            String documentId=DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String numId=documentId.split(":")[1];
+                String selection=MediaStore.Images.Media._ID+"="+numId;
+                imagePath=getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }
+            else if("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri= ContentUris.withAppendedId
+                        (Uri.parse("content://downloads//public_downloads"),Long.valueOf(documentId));
+                imagePath=getImagePath(contentUri,null);
+            }
+        }
+        else if("content".equalsIgnoreCase(uri.getScheme())){
+            imagePath=getImagePath(uri,null);
+        }
+        else if("file".equalsIgnoreCase(uri.getScheme())){
+            imagePath=uri.getPath();
+        }
+        displayImage(imagePath);
+    }
+
+    private void handleImageBeforeKitKat(Intent data){
+        Uri uri=data.getData();
+        String imagePath=getImagePath(uri,null);
+        displayImage(imagePath);
+    }
+
+    private String getImagePath(Uri imageUri,String selection){
+        String imagePath=null;
+        Cursor cursor=getContentResolver().query(imageUri,null,selection,null,null);
+        if(cursor!=null){
+            if(cursor.moveToFirst()){
+                imagePath=cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return imagePath;
+    }
+
+    private void displayImage(String imagePath){
+        if(imagePath!=null){
+            Bitmap bitmap= BitmapFactory.decodeFile(imagePath);
+            photo.setImageBitmap(bitmap);
+        }
+        else{
+            Toast.makeText(MainActivity.this,"Fail to get image",Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -67,6 +172,16 @@ public class MainActivity extends AppCompatActivity {
                     //}
                     //photo.setImageBitmap(bitmap);
                     Glide.with(MainActivity.this).load(imageUri).into(photo);
+                }
+                break;
+            case CHOOSE_PHOTO:
+                if (RESULT_OK==resultCode){
+                    if(Build.VERSION.SDK_INT>=19){
+                        handleImageOnKitKat(data);
+                    }
+                    else{
+                        handleImageBeforeKitKat(data);
+                    }
                 }
                 break;
             default:break;
